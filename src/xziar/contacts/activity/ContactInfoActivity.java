4913f2b196bccfabec9c7f66551a04782feff68c
Chen Zack <czktc2007@gmail.com>
2016-05-16 01:20:12 +0800

    simple use class to reduce code size

--- src/xziar/contacts/activity/ContactInfoActivity.java
+++ src/xziar/contacts/activity/ContactInfoActivity.java
@@ -2,45 +2,39 @@ package xziar.contacts.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import xziar.contacts.R;
 import xziar.contacts.bean.ContactBean;
+import xziar.contacts.widget.ContactInfoItem;
 
 public class ContactInfoActivity extends Activity
 {
 	private ContactBean cb;
-	private LinearLayout ll_cont, ll_cel, ll_tel,ll_email, ll_des;
+	private LinearLayout ll_cont;
+	private ContactInfoItem cii_cel, cii_tel, cii_email, cii_des;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_info);
-		((TextView) findViewById(R.id.contact_title)).setText(cb.getName());
 		cb = (ContactBean) getIntent().getSerializableExtra("ContactBean");
+		TextView name = (TextView) findViewById(R.id.contact_title);
+		name.setText(cb.getName());
 		ll_cont = (LinearLayout) findViewById(R.id.contact_content);
-		ll_cel = (LinearLayout) LayoutInflater.from(this)
-				.inflate(R.layout.contact_info_item, ll_cont, false);
-		((TextView) ll_cel.findViewById(R.id.contact_name)).setText("手机");
-		((TextView) ll_cel.findViewById(R.id.contact_val)).setText(cb.getCel());
-		ll_cont.addView(ll_cel);
-		ll_tel = (LinearLayout) LayoutInflater.from(this)
-				.inflate(R.layout.contact_info_item, ll_cont, false);
-		((TextView) ll_tel.findViewById(R.id.contact_name)).setText("座机");
-		((TextView) ll_tel.findViewById(R.id.contact_val)).setText(cb.getTel());
-		ll_cont.addView(ll_tel);
-		ll_email = (LinearLayout) LayoutInflater.from(this)
-				.inflate(R.layout.contact_info_item, ll_cont, false);
-		((TextView) ll_tel.findViewById(R.id.contact_name)).setText("Email");
-		((TextView) ll_tel.findViewById(R.id.contact_val)).setText(cb.getEmail());
-		ll_cont.addView(ll_email);
-		ll_des = (LinearLayout) LayoutInflater.from(this)
-				.inflate(R.layout.contact_info_item, ll_cont, false);
-		((TextView) ll_des.findViewById(R.id.contact_name)).setText("备注");
-		((TextView) ll_des.findViewById(R.id.contact_val))
-				.setText(cb.getDescribe());
-		ll_cont.addView(ll_des);
+
+		cii_cel = new ContactInfoItem(this, ll_cont, "手机", cb.getCel());
+		ll_cont.addView(cii_cel.getLayout());
+
+		cii_tel = new ContactInfoItem(this, ll_cont, "座机", cb.getTel(), false);
+		ll_cont.addView(cii_tel.getLayout());
+
+		cii_email = new ContactInfoItem(this, ll_cont, "Email", cb.getEmail(), false);
+		ll_cont.addView(cii_email.getLayout());
+
+		cii_des = new ContactInfoItem(this, ll_cont, "备注", cb.getDescribe(), false);
+		ll_cont.addView(cii_des.getLayout());
+
 	}
 }
